a8213d98f330ed21cdec92629f8d82a8604d8bdb
Giles Edkins <edkins@gmail.com>
2015-09-15 19:09:09 -0400

    Annotations change as you type

--- input.js
+++ input.js
@@ -1,18 +1,38 @@
-function change(i)
+var change_queued = false;
+
+function makeSenseOf(str)
+{
+  if (str == 'mathematics')
+    return 'import';
+  else
+    return '?';
+}
+
+var classes = {
+  'import' : 'import',
+  '?' : 'unknown',
+  '...' : 'editing'
+};
+
+function handleKeyDown()
+{
+  if (!change_queued)
+  {
+    setTimeout(change, 10);
+    change_queued = true;
+  }
+}
+
+function change()
 {
   var e = document.getElementById('editor');
   var range = document.createRange();
-  var dump = document.getElementById('annotations');
-
-  while (dump.firstChild)
-  {
-    dump.removeChild(dump.firstChild);
-  }
+  var dump = [];
 
   e.normalize();
   for (var n = e.firstChild; n != null; n = n.nextSibling)
   {
-    if (n instanceof Text)
+    if (n instanceof Text || n instanceof HTMLDivElement)
     {
       var str = n.textContent.trim();
       if (str != '')
@@ -22,12 +42,26 @@ function change(i)
         var rects = range.getClientRects();
 
         var an = document.createElement('span');
-        an.style.left = rects[0].left - 25;
+        an.style.left = rects[0].left - 55;
         an.style.top = rects[0].top;
-        an.textContent = str.length;
-        dump.appendChild(an);
+        var anstr = makeSenseOf(str);
+        an.textContent = anstr;
+        an.className = classes[an.textContent];
+        dump.push(an);
       }
     }
   }
+
+  var ans = document.getElementById('annotations');
+  while (ans.firstChild)
+  {
+    ans.removeChild(ans.firstChild);
+  }
+  for (var i = 0; i < dump.length; i++)
+  {
+    ans.appendChild(dump[i]);
+  }
+
+  change_queued = false;
 }
 
